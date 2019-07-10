7c28b1e872cea0e53266f2481b4876f788a2575c
Vichearith <vichearith.ros@gmail.com>
2019-07-10 10:05:31 +0200

    Nombre entre 1 et 25

--- exo_20.rb
+++ exo_20.rb
@@ -3,7 +3,13 @@ print "> "
 a = gets.chomp.to_i
 b = 1
 until b > a do
-    b.times {print "#"}
-    puts "\n"
-    b = b+1
+    if a < 1 || a > 25 
+        puts " Tu sais lire? Un nombre entre 1 et 25!"
+        print "> "
+        a = gets.chomp.to_i
+    else
+        b.times {print "#"}
+        puts "\n"
+        b = b+1   
+    end
 end
--- pyramide.rb
+++ pyramide.rb
@@ -1,12 +1,17 @@
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu"
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu entre 1 et 25 ?"
 print "> "
 a = gets.chomp.to_i
 b = 1
 until b > a do
-    c = a-b
-    c.times {print "\s"}
-    b.times {print "#"}
-    puts "\n"
-    b = b+1
-    
+    if a < 1 || a > 25 
+            puts " Tu sais lire? Un nombre entre 1 et 25!"
+            print "> "
+            a = gets.chomp.to_i
+        else
+            c = a-b
+            c.times {print "\s"}
+            b.times {print "#"}
+            puts "\n"
+            b = b+1     
+        end
 end
